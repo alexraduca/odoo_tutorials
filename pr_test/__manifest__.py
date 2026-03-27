fd60e80fb8af69aad963bcddb052085932219d8f
alexraduca <alex.raduca@prconsulting.eu>
2026-03-27 12:29:21 +0100

    first commit

---
+++ pr_test/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'PR Test',
+    'author': 'PRConsulting',
+    'application': True,
+    'installable': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    'depends': ['base'],
+    'license': 'LGPL-3'
+}
---
+++ pr_test/models/estate_property.py
@@ -0,0 +1,33 @@
+from odoo import fields, models
+from dateutil.relativedelta import relativedelta
+
+class TestModel(models.Model):
+    _name = 'estate.property'
+    _description = 'Estate Property'
+
+    name = fields.Char('Title', required=True, translate=True)
+    active = fields.Boolean('Active', default=True)
+    state = fields.Selection(
+        string='Status',
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        default='new',
+        help='Status of the property',
+        required=True,
+        copy=False
+    )
+    description = fields.Char('Description', required=True, translate=True)
+    postcode = fields.Char('Postcode', required=True)
+    date_availability = fields.Date('Available From', copy=False, default=(fields.Date.today() + relativedelta(months=3)).strftime('%Y-%m-%d'))
+    expected_price = fields.Float('Expected Price', required=True)
+    selling_price = fields.Float('Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', required=True, default=2)
+    living_area = fields.Integer('Living Area (sqm)', required=True)
+    facades = fields.Integer('Number of Facades', required=True)
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden Area (sqm)')
+    garden_orientation = fields.Selection(
+        string='Garden Orientation',
+        selection=[('north', 'North'),('south', 'South'),('east', 'East'),('west', 'West')],
+        help='Orientation of the garden'
+    )
