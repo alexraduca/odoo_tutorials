2c61a229bd546e6dbda2f920631c3ccf24452892
alexraduca <alex.raduca@prconsulting.eu>
2026-04-01 11:28:44 +0200

    Added computed fields and onchange functions

--- pr_test/models/estate_property.py
+++ pr_test/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 from dateutil.relativedelta import relativedelta
 
 class TestModel(models.Model):
@@ -37,3 +37,28 @@ class TestModel(models.Model):
         selection=[('north', 'North'),('south', 'South'),('east', 'East'),('west', 'West')],
         help='Orientation of the garden'
     )
+    total_area = fields.Integer('Total Area (sqm)', compute='_compute_total_area')
+    best_price = fields.Float('Best Offer', compute='_compute_best_price')
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for record in self:
+            if record.offer_ids:
+                record.best_price = max(record.offer_ids.mapped('price'))
+            else:
+                record.best_price = 0
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        for record in self:
+            if record.garden:
+                record.garden_area = 10
+                record.garden_orientation = 'north'
+            else:
+                record.garden_area = 0
+                record.garden_orientation = False
--- pr_test/models/estate_property_offer.py
+++ pr_test/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 from dateutil.relativedelta import relativedelta
 
 class TestModelOffer(models.Model):
@@ -11,5 +11,19 @@ class TestModelOffer(models.Model):
         selection=[('accepted', 'Accepted'), ('refused', 'Refused')],
         copy=False
     )
+    validity = fields.Integer('Validity (days)', default=7)
+    date_deadline = fields.Date('Deadline', compute='_compute_date_deadline', inverse='_inverse_date_deadline', store=True)
     partner_id = fields.Many2one('res.partner', string='Partner')
     property_id = fields.Many2one('estate.property', string='Property')
+
+    @api.depends('validity', 'create_date')
+    def _compute_date_deadline(self):
+        for record in self:
+            date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.date_deadline = date + relativedelta(days=record.validity)
+
+    @api.onchange('date_deadline')
+    def _inverse_date_deadline(self):
+        for record in self:
+            date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.validity = (record.date_deadline - date).days
